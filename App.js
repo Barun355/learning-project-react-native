0015f0c4e57b5874e2a1b816484506645acefdda
Barun355 <baruntiwary620@gmail.com>
2024-02-24 21:40:18 +0530

    7. Modal API

--- App.js
+++ App.js
@@ -1,25 +1,27 @@
-import { View, Text, Image, ScrollView, Pressable } from "react-native";
+import { useState } from "react";
+import { View, Pressable, Button, Modal } from "react-native";
 const logoImage = require("./assets/adaptive-icon.png")
 
 
 export default function App() {
+
+  const [isModalVisible, setIsModalVisisble] = useState(false)
+
   return (
     <View style={{ backgroundColor: "plumb", flex: 1, padding: 60 }}>
-      <ScrollView>
-        <Pressable onPressIn={() => console.log("In Image")} onPressOut={() => console.log("Out Image")} onPress={() => console.log("image pressed")}>
-          <Image source={logoImage} style={{ width: 300, height: 300 }} />
-        </Pressable>
-        <Pressable onPress={() => console.log("text pressed")}>
-          <Text>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-          </Text>
-        </Pressable>
-        <Pressable onPressIn={() => console.log("In Image")} onPressOut={() => console.log("Out Image")} onLongPress={() => console.log("Long pressed")}>
-          <Text>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-          </Text>
-        </Pressable>
-      </ScrollView>
+        <Button
+          title="Press"
+          color="midnightblue"
+          onPress={() => {
+            console.log("Button  Pressed")
+            setIsModalVisisble(true)
+          }}
+        />
+        <Modal visible={isModalVisible} onRequestClose={() =>  setIsModalVisisble(false)} animationType="slide">
+          <View style={{flex: 1, backgroundColor: "lightblue", padding: 60}}>
+            <Button title="Close" color="midnightblue" onPress={() => setIsModalVisisble(false)}/>
+          </View>
+        </Modal>
     </View>
   )
 }
