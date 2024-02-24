100d1ec70eae17e213cc6457059ba29d9e386fef
Barun355 <baruntiwary620@gmail.com>
2024-02-24 17:38:36 +0530

    5. Button API

--- App.js
+++ App.js
@@ -1,17 +1,11 @@
-import { View, Text, Image, ScrollView } from "react-native";
+import { View, Button } from "react-native";
 const logoImage = require("./assets/adaptive-icon.png")
 
 
 export default function App() {
   return (
     <View style={{ backgroundColor: "plumb", flex: 1, padding: 60 }}>
-      <ScrollView>
-        <Image source={logoImage} style={{ width: 300, height: 300 }} />
-        <Text>
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-        </Text>
-        <Image source={{ uri: "https://picsum.photos/300" }} style={{ width: 300, height: 300 }} />
-      </ScrollView>
+      <Button title="Press" onPress={() => console.log("Button Pressed")} color="midnightblue" disabled />
     </View>
   )
 }
